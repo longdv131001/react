1ff00d717fc37092c4f41524eee0dc109db134d3
longdv131001 <vietlong13102001@gmail.com>
2021-11-02 15:26:02 +0700

    2 files modified, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Heroes from './components/Heroes/Heroes';
+import React from 'react';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Tour of Heroes</h1>
-        <h2>My Heroes</h2>
-        <Heroes />
-        
+      <h2>My Heroes</h2>
+      <Heroes />
     </div>
   );
-}
+};
 
 export default App;
